4efabb11bf7d96e09e1f040afd65b4c02d94d452
ziad <ziad.alsabouni@outlook.com>
2020-08-05 15:51:41 +0300

    redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Provider store={store}>
+                  <App />
+          </Provider>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
